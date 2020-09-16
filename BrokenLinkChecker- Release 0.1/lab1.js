de06dbc7f7f40d1d610945da942a5391bb72fcf6
joey2031 <36198917+joey2031@users.noreply.github.com>
2020-09-16 14:26:37 -0400

    Add files via upload

--- BrokenLinkChecker- Release 0.1/lab1.js
+++ BrokenLinkChecker- Release 0.1/lab1.js
@@ -1,54 +1,52 @@
 // Built in node functionality to get command line args. Src: https://stackabuse.com/command-line-arguments-in-node-js/#:~:text=command()%20option%2C%20which%20helps,argv)%20%3D%3E%20%7B%20console. 
+// Beginner guide to RegEx: https://blog.bitsrc.io/a-beginners-guide-to-regular-expressions-regex-in-javascript-9c58feb27eb4
 // RegEx checker with regEx for http and https links: https://www.regexpal.com/?fam=104034
 // Top 15 Commonly Used Regex: https://digitalfortress.tech/tricks/top-15-commonly-used-regex/
 
+/*
+2 ways to make a regular expression in Javascript.
+var regexConst = new RegExp('abc');
+var regexLiteral = /abc/;
+*/
 
 /*first index contains the path to our node executable, second index contains the path to the script file
 rest of the indexes contain the arguments that we passed in their respective sequence.*/
-
-let fs = require('fs');
+const fetch = require("node-fetch");
+const fs = require('fs');
+const colors = require('colors');
 let linesArr = [];
 let linkArr = [];
 let regEx = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#()?&//=]*)/igm
 
 
-
-console.log(process.argv.length); 
 if (process.argv.length < 3) { // will always be at least 2
     console.log("ERROR: Please enter command line argument (name of file to be processed).");
 } else {
 
     // Each index in the array stores a line in the file
     linesArr = fs.readFileSync(process.argv[2], 'utf8').split('\n');
-
     for (let i = 0; i < linesArr.length; i++) {
-        console.log(linesArr[i]);
+        if (regEx.test(linesArr[i])) { // if its true (match has been found)
+            // exec did not work, had to use match instead
+            var result = linesArr[i].match(regEx); // store the match. **Create and populate variable**
+            linkArr.push(result); // push into array 
+        }
     }
 
-    for (let i = 0; i < linesArr.length; i++) {
-       if(regEx.test(linesArr[i])){ // if its true (match has been found)
-        //console.log("The link found is " + linesArr[i]);
-         // console.log(regEx.exec(linesArr[i])); -> null for some reason-> This did not work, had to use match instead
-
-        var result = linesArr[i].match(regEx); // store the match. **Create and populate variable**
-        linkArr.push(result); // push into array 
-       }
-    }
-    
-    console.log("Going to print linkArr!");
     for (let i = 0; i < linkArr.length; i++) {
-        console.log(linkArr[i]);
+        fetch(linkArr[i]).then(response => {
+            if (response.status == 200) { // good
+                console.log(`${linkArr[i]} was good! status: ${response.status}`.green);
+            } else if (response.status == 404 || response.status == 401) { // bad
+                console.log(`${linkArr[i]} was bad! status: ${response.status}`.red);
+            } else { // unknown
+                console.log(`${linkArr[i]} was unknown! status: ${response.status}`.gray);
+            }
+        })
     }
 }
 
 
-/*
-Src: https://blog.bitsrc.io/a-beginners-guide-to-regular-expressions-regex-in-javascript-9c58feb27eb4
-2 ways to make a regular expression in Javascript.
-var regexConst = new RegExp('abc');
-var regexLiteral = /abc/;
-
-*/
 
 
 
