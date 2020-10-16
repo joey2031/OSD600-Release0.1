b361380daab70e0117dfc26086f4a84c2cf9c36f
Jenn <39388566+strawberries73@users.noreply.github.com>
2020-10-16 08:34:24 -0400

    Update index.js with --ignore feature

--- BrokenLinkChecker- Release 0.1/index.js
+++ BrokenLinkChecker- Release 0.1/index.js
@@ -21,44 +21,44 @@ const { resolve } = require('path');
 const dnsPromise = util.promisify(dns.resolve);
 const rrtype = "AAAA" //IPv6 address
 let linesArr = [];
+let linkArr = [];
+let ignoreLinks = [];
 let regEx = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#()?&//=]*)/igm
 
-//console.log(process.argv[3]);
 
 if (process.argv.length < 3) { // will always be at least 3
     console.log("ERROR: Please enter command line argument (name of file to be processed).");
 } else {
     if (process.argv[2] == "v" || process.argv[2] == "version") {
         console.log(packageJson.name + " Version " + packageJson.version);
-    } else {
-        // Each index in the array stores a line in the file
-        linesArr = fs.readFileSync(process.argv[2], 'utf8').split('\n');
-        const linkArr = [];
-        for (let i = 0; i < linesArr.length; i++) {
-            if (regEx.test(linesArr[i])) { // if its true (match has been found)
-                var result = linesArr[i].match(regEx); // store the match. **Create and populate variable** (exec did not work, had to use match instead)
-
-                // If result.length is > then 1 (have more then one link on one line) we need to grab each index.
-                if (result.length > 1) {
-                    for (let i = 0; i < result.length; i++) {
-                        linkArr.push(result[i]);
-                    }
+    } else if (process.argv[2] == "--ignore"){
+        //file with ignore links, argv[4] will be the link to process
+        const file = process.argv[3];
+        ignoreLinks = fs.readFileSync(file, 'utf-8').split('\n');
+        ignoreLinks = ignoreLinks.filter(w => !w.startsWith('#'));
+        ignoreLinks = ignoreLinks.filter(w => !w.startsWith("www."));
+        //console.log(ignoreLinks);
+        //console.log(ignoreLinks.length);
+        if(ignoreLinks.length == 0){
+            console.log("Invalid file, file cannt contain only comments (#) and invalid URLs")
+            process.exitCode = 1;
+        } else{
+            populateLinkArr();
+            makeCalls(linkArr).then((data) => { // pass in linkArr return data (array of booleans)
+                if (data.every(result => result === true)) {
+                    console.log("exit with 0 for good - Note if using flag output of links may not be accurate");
+                    process.exitCode = 0; // better way instead of  process.exit() 
                 } else {
-                    linkArr.push(result); // push into array 
+                    console.log("exit with 1 for bad- Note if using flag output of links may not be accurate");
+                    process.exitCode = 1;
                 }
-            }
+            }).catch((err) => {
+                console.log(err)
+            });
         }
-        /*
-           The every() method tests whether all elements in the array pass the test
-           In this case check each result in the array is == true 
-           console.log("All Good", data.every(result => result === true))
 
-           Note: Calling process.exit() will force the process to exit as quickly as possible even if there
-           are still asynchronous operations pending that have not yet completed fully, including I/O operations
-           to process.stdout and process.stderr. In most situations, it is not actually necessary to call
-           process.exit() explicitly src: https://nodejs.org/api/process.html#process_process_exit_code
-        */
-
+    } else {
+        populateLinkArr();
         makeCalls(linkArr).then((data) => { // pass in linkArr return data (array of booleans)
             if (data.every(result => result === true)) {
                 console.log("exit with 0 for good - Note if using flag output of links may not be accurate");
@@ -71,6 +71,7 @@ if (process.argv.length < 3) { // will always be at least 3
             console.log(err)
         });
     }
+
 }
 
 // Promise.all will take an array of promises and returns a single Promise that 
@@ -88,23 +89,58 @@ async function processLink(link) {
     try {
         const response = await fetch(link, { method: "HEAD" });
         let isGood = false;
-        if (process.argv[3] == "--good") {
-            if (response.status == 200) { // good
-                console.log(`${link} was good! status: ${response.status}`.green);
-                isGood = true;
-            }
-        } else if (process.argv[3] == "--bad") {// Here we dont care about the good ones so no need to worry about isGood
-            if (response.status == 404 || response.status == 401) { // bad
-                console.log(`${link} was bad! status: ${response.status}`.red);
-            }
-        } else { // all
-            if (response.status == 200) { // good
-                console.log(`${link} was good! status: ${response.status}`.green);
-                isGood = true;
-            } else if (response.status == 404 || response.status == 401) { // bad
-                console.log(`${link} was bad! status: ${response.status}`.red);
-            } else { // unknown
-                console.log(`${link} was unknown! status: ${response.status}`.gray);
+
+        if(ignoreLinks.length != 0){
+            console.log("joey");
+            console.log("ignore links");
+            console.log(ignoreLinks);
+
+            // Need to trim since every line ends with \r
+            var trimmedArr = ignoreLinks.map(s => s.trim())
+            console.log("trimmedArr:");
+            console.log(trimmedArr);
+
+        // Stack overflow: https://stackoverflow.com/questions/34901593/how-to-filter-an-array-from-all-elements-of-another-array
+        // Filter using the trimmed array since that matches the links grabed by the RegEx
+        var filtered = linkArr.filter(f => !trimmedArr.includes(f)) // f repersents ignoreLinks
+        console.log("filtered");
+        console.log(filtered);
+        // call for each on new array with filter on it
+        filtered.forEach((link)=>{
+            fetch(link,{method:'HEAD',timeout:2000})
+            .then((res)=>{
+                if(res.status==200){
+                    console.log(`INSIDE FILTERED.FOREACH${link} was good! status: ${response.status}`.green);
+                }
+                
+                if(res.status==400||res.status==404){
+                    console.log(`INSIDE FILTERED.FOREACH ${link} was bad! status: ${response.status}`.red);
+                }
+
+            }).catch((error)=>{
+                console.log("404",link.red)
+            })
+        })
+        } else{ // check for --good --bad
+            if (process.argv[3] == "--good") {
+                if (response.status == 200) { // good
+                    console.log(`${link} was good! status: ${response.status}`.green);
+                    isGood = true;
+                }
+            } else if (process.argv[3] == "--bad") {// Here we dont care about the good ones so no need to worry about isGood
+                if (response.status == 404 || response.status == 401) { // bad
+                    console.log(`${link} was bad! status: ${response.status}`.red);
+                }
+            } else { // all
+                if (response.status == 200) { // good
+                    console.log(`${link} was good! status: ${response.status}`.green);
+                    isGood = true;
+                } else if (response.status == 404 || response.status == 401) { // bad
+                    console.log(`${link} was bad! status: ${response.status}`.red);
+                } else { // unknown
+                    console.log(`${link} was unknown! status: ${response.status}`.gray);
+                }
+    
             }
 
         }
@@ -114,4 +150,31 @@ async function processLink(link) {
     } catch (err) {
         console.log(err);
     }
+}
+
+function populateLinkArr(){
+    let processFile = "";
+    if(process.argv[2] == "--ignore"){ // file position changes depending if we have --igronr oe not
+        processFile = process.argv[4];
+    } else{
+        processFile = process.argv[2];
+    }
+
+     // Each index in the array stores a line in the file
+     linesArr = fs.readFileSync(processFile, 'utf8').split('\n');
+     for (let i = 0; i < linesArr.length; i++) {
+         if (regEx.test(linesArr[i])) { // if its true (match has been found)
+             var result = linesArr[i].match(regEx); // store the match. **Create and populate variable** 
+
+             // If result.length is > then 1 (have more then one link on one line) we need to grab each index.
+             if (result.length > 1) {
+                 for (let i = 0; i < result.length; i++) {
+                     linkArr.push(result[i]);
+                 }
+             } else {
+                 linkArr.push(result); // push into array 
+             }
+         }
+     }
+            
 }
